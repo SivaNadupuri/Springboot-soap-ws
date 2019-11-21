b7353cebc122afb760bd3d7c9d67393f6cfff746
SivaNadupuri <40888140+SivaNadupuri@users.noreply.github.com>
2019-11-21 16:59:26 +0530

    Add files via upload

---
+++ src/test/java/com/siva/spring/soap/api/SpringbootSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.spring.soap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
